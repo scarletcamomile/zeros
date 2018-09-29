a0fd244d12418439a7421c680668105f266dfe8f
scarletcamomile <scarletcamomile@mail.ru>
2018-09-30 01:07:09 +0300

    clear solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  let fives = 5;
+  let zeros = 0, zero = 0;
+  for (;;) {
+    zero = Math.floor(number/fives);
+    if (zero < 1) break;
+    zeros += zero;
+    fives *= 5;
+  }
+  return zeros;
+  }
